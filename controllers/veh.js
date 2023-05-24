d4fbe757f97b5be6d00b139a71014896fa2a369f
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2023-05-23 19:00:09 -0500

    L06

--- controllers/veh.js
+++ controllers/veh.js
@@ -2,22 +2,38 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db('project-portfolio').collection('vehicles').find();
-  result.toArray().then((lists) => {
+  mongodb
+  .getDb()
+  .db('project-portfolio')
+  .collection('vehicles')
+  .find()
+  .toArray((err, lists) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
 };
 
 const getSingle = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to find a contact.');
+  }
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db('project-portfolio').collection('vehicles').find({ _id: userId });
-  result.toArray().then((lists) => {
+  mongodb
+  .getDb()
+  .db('project-portfolio')
+  .collection('vehicles')
+  .find({ _id: userId })
+  toArray((err, result) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    }
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
+    res.status(200).json(result[0]);
   });
 };
-
 const createveh = async (req, res) => {
   const veh = {
     Veh_number: req.body.Veh_number,
