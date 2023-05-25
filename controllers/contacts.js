3532c5ef55138f88422a9223aeffcdee16bfcfee
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2023-05-25 13:11:30 -0500

    test

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,9 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res) => {
   const result = await mongodb.getDb().db('project-portfolio').collection('users').find();
   result.toArray().then((lists) => {
-    if (err) {
-      res.status(400).json({ message: err });
-    }
+
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
@@ -17,6 +15,9 @@ const getSingle = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db('project-portfolio').collection('users').find({ _id: userId });
   result.toArray().then((lists) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
