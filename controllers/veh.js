a01f2215182c184c83b53b0803d94ef9eb169362
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2023-05-25 18:33:03 -0500

    sabve

--- controllers/veh.js
+++ controllers/veh.js
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid contact id to find a contact.');
+    res.status(400).json('Must use a valid vehicle id to find a contact.');
   }
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db('project-portfolio').collection('vehicles').find({ _id: userId });
